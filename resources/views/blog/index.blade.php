d67a5d8b98935da2042788af3ebff3d74d48ec77
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-05-20 18:31:11 +0700

    feat (style) : add bootstrap css

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
     <title>Blog</title>
+
 </head>
+
 <body>
     <div class="container">
         <h1>Tes</h1>
     </div>
 
 
+    <!-- SCRIPT -->
+    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
 </body>
+
 </html>
