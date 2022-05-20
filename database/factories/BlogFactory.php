3bdc0465dc606ee02bda18bd1efec6c952ed7b65
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-05-20 21:00:17 +0700

    feat (db) : create faker n seeder

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->unique()->sentence(3);
+        $slug = Str::slug($title);
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'body' => $this->faker->paragraph,
+
+        ];
+    }
+}
