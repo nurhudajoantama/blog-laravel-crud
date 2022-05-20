890b5009c8291f8d9272e1f91436b4d518f0dd29
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-05-20 21:08:16 +0700

    feat (validation) : add slug validation

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Blog;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
@@ -31,9 +32,9 @@ class BlogController extends Controller
             'slug' => 'required|unique:blogs',
             'body' => 'required',
         ]);
-
+        $request->merge(['slug' => Str::slug($request->slug)]);
+        dd($request->all());
         Blog::create($request->all());
-
         return redirect()->route('blog.index');
     }
 
