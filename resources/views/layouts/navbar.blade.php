de2de32cf69741d34ad5fff33bf5239a8f6b7814
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-05-21 10:15:44 +0700

    style (navbar) : fix navbar link

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,45 +1,48 @@
 <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container d-flex justify-content-between">
+    <div class="container">
 
-        <div class="d-flex">
-            <a class="navbar-brand" href="{{route('index')}}">CRUD Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('blog.index')}}">Blog</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div>
-            <ul class="navbar-nav">
-                @auth
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        {{auth()->user()->name}}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="{{route('dashboard.index')}}">Dashboard</a></li>
-                        <li>
-                            <form action="{{route('logout')}}" method="POST">
-                                @csrf
-                                <button class="dropdown-item" type="submit">Logout</button>
-                            </form>
+
+        <a class="navbar-brand" href="{{route('index')}}">CRUD Blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex justify-content-between w-100">
+                <div>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('blog.index')}}">Blog</a>
                         </li>
                     </ul>
-                </li>
-                @else
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}">Login</a>
-                </li>
-                @endauth
-            </ul>
+                </div>
+                <div>
+                    <ul class="navbar-nav">
+                        @auth
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                {{auth()->user()->name}}
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{route('dashboard.index')}}">Dashboard</a></li>
+                                <li>
+                                    <form action="{{route('logout')}}" method="POST">
+                                        @csrf
+                                        <button class="dropdown-item" type="submit">Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('login')}}">Login</a>
+                        </li>
+                        @endauth
+                    </ul>
+
+                </div>
+            </div>
         </div>
     </div>
 </nav>
